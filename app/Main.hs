0105183b74f598450978c61bac4093d0305bbf88
Joseph Canero <jmc41493@gmail.com>
2018-07-12 00:19:36 -0400

    make it easier to change width and height

--- app/Main.hs
+++ app/Main.hs
@@ -15,12 +15,21 @@ import           Text.Read
 --- Graphics
 ---
 
-width, height :: Int
-width = 200
-height = 200
+squareSize :: Float
+squareSize = 25
+
+width, height, squaresPerRow, squaresPerCol :: Int
+width = 800
+height = 700
+squaresPerRow = floor (fromIntegral height / squareSize)
+squaresPerCol = floor (fromIntegral width / squareSize)
+
+upperLeftX, upperLeftY :: Float
+upperLeftX = negate $! (fromIntegral width / 2) - (squareSize / 2)
+upperLeftY = (fromIntegral height / 2) - (squareSize / 2)
 
 upperLeft :: (Float, Float)
-upperLeft = (-87.5, 87.5)
+upperLeft = (upperLeftX, upperLeftY)
 
 tx :: (Float, Float) -> Picture -> Picture
 tx = uncurry Translate
@@ -30,7 +39,7 @@ toUpperLeft = tx upperLeft
 
 rect :: Bool -> Picture
 rect living =
-    let rect = rectangleSolid 25 25
+    let rect = rectangleSolid squareSize squareSize
         in if living
             then color white rect
             else color black rect
@@ -40,8 +49,8 @@ worldToPicture world = V.ifoldl' buildPictureForRow mempty world
     where
         buildPictureForRow picture row cols = picture <> V.ifoldl' (buildCellPicture row) mempty cols
         buildCellPicture row picture col cell =
-            let ypos = 87.5 + (-25 * fromIntegral row)
-                xpos = -87.5 + (25 * fromIntegral col)
+            let ypos = upperLeftY + (-squareSize * fromIntegral row)
+                xpos = upperLeftX + (squareSize * fromIntegral col)
                 in picture `mappend` tx (xpos, ypos) (rect cell)
 
 defaultFPS :: Int
@@ -71,16 +80,16 @@ livingNeighbors x y world = length . filter id $! mapMaybe toCell indices
         toCell (xloc, yloc) = (world !? yloc) >>= \v -> v !? xloc
 
 seedWorld :: IO World
-seedWorld = V.unfoldrNM 8 (\_ -> Just . (,0) <$> randomBooleanVector) 0
+seedWorld = V.unfoldrNM squaresPerRow (\_ -> Just . (,0) <$> randomBooleanVector) 0
 
 randomBooleanVector :: IO (V.Vector Bool)
-randomBooleanVector = fmap V.fromList . sequence . take 8 $! repeat randomIO
+randomBooleanVector = fmap V.fromList . sequence . take squaresPerCol $! repeat randomIO
 
 gameOfLife :: Int -> World -> IO ()
-gameOfLife fps world = simulate (InWindow "Game of Life" (200, 200) (10, 10)) black fps world worldToPicture (\_ _ w -> evolve w)
+gameOfLife fps world = simulate (InWindow "Game of Life" (width, height) (10, 10)) black fps world worldToPicture (\_ _ w -> evolve w)
 
 staticWorld :: World -> IO ()
-staticWorld world = display (InWindow "Game of Life" (200, 200) (10, 10)) black (worldToPicture world)
+staticWorld world = display (InWindow "Game of Life" (width, height) (10, 10)) black (worldToPicture world)
 
 main = do
     fps <- fromMaybe defaultFPS . join . fmap readMaybe . listToMaybe <$> getArgs
